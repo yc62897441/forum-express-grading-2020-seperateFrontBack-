e49cc90ee6f503dfb84ed9aa6e76a00eb640367f
yc62897441 <yc62897441@gmail.com>
2022-05-18 10:34:40 +0800

    refactor router

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app, passport)
+require('./routes')(app)
 // 上面那一行等於下面這兩行的意思。
 // 要注意 require('./routes')(app) 需要放在 app.js 的最後一行，因為按照由上而下的順序，當主程式把 app (也就是 express() ) 傳入路由時，程式中間做的樣板引擎設定、伺服器設定，也要一併透過 app 變數傳進去。
 // const router = require('./routes')
