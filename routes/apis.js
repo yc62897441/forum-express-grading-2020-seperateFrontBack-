7b6702b713197e8bf6ab1d5bbca31f02d106e5a5
yc62897441 <yc62897441@gmail.com>
2022-05-19 13:14:00 +0800

    add authenticated and authenticatedAdmin

--- routes/apis.js
+++ routes/apis.js
@@ -11,40 +11,51 @@ const userController = require('../controllers/api/userController.js')
 const multer = require('multer')
 const upload = multer({ dest: 'temp/' })
 
-const authenticated = (req, res, next) => {
-  // 開發時的寫法註解起來
-  // if (req.isAuthenticated()) {
-  // 改用下方測試用的 _helper.js
-  if (helpers.ensureAuthenticated(req)) {
-    return next()
-  }
-  return res.redirect('/signin')
-}
+const authenticated = passport.authenticate('jwt', { session: false })
+
 const authenticatedAdmin = (req, res, next) => {
-  // 開發時的寫法註解起來
-  // if (req.isAuthenticated()) {
-  //   if (req.user.isAdmin) {
-  // 改用下方測試用的 _helper.js
-  if (helpers.ensureAuthenticated(req)) {
-    if (helpers.getUser(req).isAdmin) {
-      return next()
-    }
-    return redirect('/')
+  if (req.user) {
+    if (req.user.isAdmin) { return next() }
+    return res.json({ status: 'error', message: 'permission denied' })
+  } else {
+    return res.json({ status: 'error', message: 'permission denied' })
   }
-  return res.redirect('/signin')
 }
 
-router.get('/admin/restaurants', adminController.getRestaurants)
-router.post('/admin/restaurants', upload.single('image'), adminController.postRestaurant)
-router.get('/admin/restaurants/:id', adminController.getRestaurant)
-router.put('/admin/restaurants/:id', upload.single('image'), adminController.putRestaurant)
-router.delete('/admin/restaurants/:id', adminController.deleteRestaurant)
+// const authenticated = (req, res, next) => {
+//   // 開發時的寫法註解起來
+//   // if (req.isAuthenticated()) {
+//   // 改用下方測試用的 _helper.js
+//   if (helpers.ensureAuthenticated(req)) {
+//     return next()
+//   }
+//   return res.redirect('/signin')
+// }
+// const authenticatedAdmin = (req, res, next) => {
+//   // 開發時的寫法註解起來
+//   // if (req.isAuthenticated()) {
+//   //   if (req.user.isAdmin) {
+//   // 改用下方測試用的 _helper.js
+//   if (helpers.ensureAuthenticated(req)) {
+//     if (helpers.getUser(req).isAdmin) {
+//       return next()
+//     }
+//     return redirect('/')
+//   }
+//   return res.redirect('/signin')
+// }
 
-router.get('/admin/categories', categoryController.getCategories)
-router.post('/admin/categories', categoryController.postCategory)
-router.get('/admin/categories/:id', categoryController.getCategories)
-router.put('/admin/categories/:id', categoryController.putCategory)
-router.delete('/admin/categories/:id', categoryController.deleteCategory)
+router.get('/admin/restaurants', authenticated, authenticatedAdmin, adminController.getRestaurants)
+router.post('/admin/restaurants', authenticated, authenticatedAdmin, upload.single('image'), adminController.postRestaurant)
+router.get('/admin/restaurants/:id', authenticated, authenticatedAdmin, adminController.getRestaurant)
+router.put('/admin/restaurants/:id', authenticated, authenticatedAdmin, upload.single('image'), adminController.putRestaurant)
+router.delete('/admin/restaurants/:id', authenticated, authenticatedAdmin, adminController.deleteRestaurant)
+
+router.get('/admin/categories', authenticated, authenticatedAdmin, categoryController.getCategories)
+router.post('/admin/categories', authenticated, authenticatedAdmin, categoryController.postCategory)
+router.get('/admin/categories/:id', authenticated, authenticatedAdmin, categoryController.getCategories)
+router.put('/admin/categories/:id', authenticated, authenticatedAdmin, categoryController.putCategory)
+router.delete('/admin/categories/:id', authenticated, authenticatedAdmin, categoryController.deleteCategory)
 
 router.post('/signin', userController.signIn)
 
